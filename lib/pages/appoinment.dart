c39c07d4ab7655f6065071a87ab02362b7c9129f
Barath Domiyan <47963290+BDomiyan@users.noreply.github.com>
2020-10-04 03:29:03 +0530

    QR done

--- lib/pages/appoinment.dart
+++ lib/pages/appoinment.dart
@@ -1,5 +1,6 @@
 import 'package:bubbletest/extra/reservation.dart';
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class Appoinment extends StatefulWidget {
   final Reservation _reservation;
@@ -15,13 +16,28 @@ class _AppoinmentState extends State<Appoinment> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('DraggableScrollableSheet'),
-      ),
+      backgroundColor: Colors.purple,
+      // appBar: AppBar(
+      //   title: const Text('DraggableScrollableSheet'),
+      // ),
       body: SizedBox.expand(
         child: Stack(
           children: <Widget>[
-            FlutterLogo(size: 500),
+            Center(
+                child: Card(
+                    color: Colors.white,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    elevation: 20,
+                    child: Container(
+                      child: QrImage(
+                        data: makeEncript(_reservation.resID),
+                        version: QrVersions.auto,
+                        size: 300.0,
+                        backgroundColor: Colors.white,
+                        padding: EdgeInsets.all(10),
+                      ),
+                    ))),
             SizedBox.expand(
               child: DraggableScrollableSheet(
                 initialChildSize: 0.05,
@@ -29,7 +45,7 @@ class _AppoinmentState extends State<Appoinment> {
                 builder:
                     (BuildContext context, ScrollController scrollController) {
                   return Container(
-                    color: Colors.blue[800],
+                    color: Colors.white,
                     child: ListView.builder(
                       controller: scrollController,
                       itemCount: 25,
@@ -45,5 +61,13 @@ class _AppoinmentState extends State<Appoinment> {
         ),
       ),
     );
+  }
+
+  String makeEncript(int x) {
+    print(x);
+    int val = 99 * x;
+    val = val + 60;
+    print(val);
+    return val.toString();
   }
 }
