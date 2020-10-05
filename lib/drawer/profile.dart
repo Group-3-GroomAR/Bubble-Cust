8ea87de0b62ca07f5a8103041559eaf463354e56
sanaucsc <36620116+sanaucsc@users.noreply.github.com>
2020-10-05 21:54:36 +0530

    Profile page Updated

--- lib/drawer/profile.dart
+++ lib/drawer/profile.dart
@@ -1,46 +1,58 @@
+import 'package:bubbletest/backend/http.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:nice_button/nice_button.dart';
-
 class Profile extends StatefulWidget {
   Profile({Key key}) : super(key: key);
-
   @override
   _ProfileState createState() => _ProfileState();
 }
-
 class _ProfileState extends State<Profile> {
   var firstColor = Colors.deepPurple, secondColor = Colors.purple;
+  String _name = "Name";
+  String _district = "District";
+  int _phone = 94;
+  String _gender = "Gender";
+  String _customerId = "cus2";
+  @override
+  void initState() {
+    // TODO: implement initState
+    getCustomer();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-//    appBar: AppBar(
-//    title: Text("Profile"),
-//   ),
       body: ListView(
         children: <Widget>[
           Card(
             child: Container(
-              margin: EdgeInsets.symmetric(horizontal: 10),
+              margin: EdgeInsets.symmetric(horizontal: 10, vertical: 50),
               child: Column(
                 children: <Widget>[
+                  // Container(
+                  //   margin: EdgeInsets.all(10),
+                  //   width: 200,
+                  //   height: 200,
+                  //   decoration: BoxDecoration(
+                  //     image: DecorationImage(
+                  //       image: AssetImage('assets/images/profile.jpg'),
+                  //     ),
+                  //     shape: BoxShape.circle,
+                  //     border: Border.all(
+                  //       color: Colors.deepPurple,
+                  //       width: 3,
+                  //     ),
+                  //   ),
+                  // ),
                   Container(
-                    margin: EdgeInsets.all(10),
-                    width: 200,
-                    height: 200,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage('assets/images/profile.jpg'),
-                      ),
-                      shape: BoxShape.circle,
-                      border: Border.all(
-                        color: Colors.deepPurple,
-                        width: 3,
-                      ),
+                    child: Icon(
+                      Icons.person,
+                      size: 200,
+                      color: Colors.deepPurple,
                     ),
                   ),
                   Text(
-                    "Sundhar Pichchai",
+                    _name,
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
                       color: Colors.deepPurple,
@@ -49,28 +61,6 @@ class _ProfileState extends State<Profile> {
                   ),
                   SizedBox(
                     height: 15,
-                  ),
-                  Container(
-                    padding: const EdgeInsets.only(left: 15, top: 30),
-                    child: Row(
-                      children: <Widget>[
-                        Icon(
-                          Icons.mail_outline,
-                          color: Colors.deepPurple,
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text(
-                          "sundharsana@gmail.com",
-                          style: TextStyle(
-                            fontWeight: FontWeight.w600,
-                            color: Colors.deepPurple,
-                            fontSize: 22,
-                          ),
-                        ),
-                      ],
-                    ),
                   ),
                   SizedBox(
                     height: 15,
@@ -87,7 +77,7 @@ class _ProfileState extends State<Profile> {
                           width: 10,
                         ),
                         Text(
-                          "0771234567",
+                          _phone.toString(),
                           style: TextStyle(
                             fontWeight: FontWeight.w600,
                             color: Colors.deepPurple,
@@ -112,7 +102,7 @@ class _ProfileState extends State<Profile> {
                           width: 10,
                         ),
                         Text(
-                          "Jaffna",
+                          _district,
                           style: TextStyle(
                             fontWeight: FontWeight.w600,
                             color: Colors.deepPurple,
@@ -137,7 +127,7 @@ class _ProfileState extends State<Profile> {
                           width: 10,
                         ),
                         Text(
-                          "Men",
+                          _gender == 'M' ? "Male" : "Female",
                           style: TextStyle(
                             fontWeight: FontWeight.w600,
                             color: Colors.deepPurple,
@@ -159,9 +149,7 @@ class _ProfileState extends State<Profile> {
           ),
         ],
       ),
-
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-
       floatingActionButton: NiceButton(
         background: Colors.black,
         radius: 40,
@@ -172,5 +160,33 @@ class _ProfileState extends State<Profile> {
         onPressed: () {},
       ),
     );
+  }
+  //this function is to get the profile details
+  Future<void> getCustomer() async {
+    var result = await httpGet('customerdetail', {
+      "customerId": _customerId,
+    });
+    if (result.ok) {
+      var inCust = result.data as List<dynamic>;
+      var inCustx = inCust[0];
+      setState(() {
+        _name = inCustx[0]['customer_name'];
+        _gender = inCustx[0]['customer_gender'];
+        _district = inCustx[0]['customer_district'];
+        _phone = inCustx[0]['customer_phone'];
+      });
+    } else {
+      print("Unable to get data");
+    }
+    // _shop = Shop.contact(
+    //     inShopx[0]['salon_id'],
+    //     inShopx[0]['shop_name'],
+    //     inShopx[0]['salon_address'],
+    //     inShopx[0]['district'],
+    //     inShopx[0]['contact'],
+    //     inShopx[0]['email'],
+    //     inShopx[0]['about']);
+    //print(inShopx[0]['salon_id']);
+    // print(_reservation.salon_id);
   }
 }
